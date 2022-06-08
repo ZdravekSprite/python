4ff1b24f2e9c112e0e14e7b0e8e2eed70ce4e7f7
Zdravko Šplajt <zdravek.sprite@gmail.com>
2022-06-08 23:51:16 +0200

    OpenCV 0.0.5

--- OpenCV/Detector.py
+++ OpenCV/Detector.py
@@ -19,9 +19,50 @@ class Detector:
         self.net.setInputSwapRB(True)
 
         self.readClasses()
-        
+
     def readClasses(self):
         with open(self.classesPath, 'r') as f:
             self.classesList =f.read().splitlines()
         
+        self.classesList.insert(0, '__Background__')
+        
         print(self.classesList)
+    
+    def onVideo(self):
+        cap = cv2.VideoCapture(self.videoPath)
+
+        if (cap.isOpened()==False):
+            print("Error opening file...")
+            return
+        
+        (success, image) = cap.read()
+
+        while success:
+            classLabelIDs, confidences, bboxs = self.net.detect(image, confThreshold = 0.5)
+
+            bboxs = list(bboxs)
+            confidences = list(np.array(confidences).reshape(1,-1)[0])
+            confidences = list(map(float, confidences))
+
+            bboxIdx = cv2.dnn.NMSBoxes(bboxs, confidences, score_threshold = 0.5, nms_threshold = 0.2)
+
+            if len(bboxIdx) !=0:
+                for i in range(0, len(bboxIdx)):
+
+                    bbox = bboxs[np.squeeze(bboxIdx[i])]
+                    classConfidence = confidences[np.squeeze(bboxIdx[i])]
+                    classLabelID = np.squeeze(classLabelIDs[np.squeeze(bboxIdx[i])])
+                    classLabel = self.classesList[classLabelID]
+
+                    x,y,w,h = bbox
+
+                    cv2.rectangle(image, (x,y), (x+w, y+h), color=(255,255,255), thickness=1)
+            
+            cv2.imshow("Result", image)
+
+            key = cv2.waitKey(1) & 0xFF
+            if key == ord("q"):
+                break
+
+            (success, image) = cap.read()
+        cv2.destroyAllWindows()
--- OpenCV/main.py
+++ OpenCV/main.py
@@ -2,13 +2,14 @@ from Detector import *
 import os
 
 def main():
-    videoPath = "test.mp4"
+    videoPath = "OpenCV/test.mp4"
 
     configPath = os.path.join("OpenCV/model_data", "ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")
     modelPath = os.path.join("OpenCV/model_data", "frozen_inference_graph.pb")
     classesPath = os.path.join("OpenCV/model_data", "coco.names")
 
-    Detector(videoPath, configPath, modelPath, classesPath)
+    detector = Detector(videoPath, configPath, modelPath, classesPath)
+    detector.onVideo()
 
 if __name__ == '__main__':
     main()
