463c74bdcb8850bdfda5e91d7f231b47350e80c3
Zdravko Šplajt <zdravek.sprite@gmail.com>
2022-06-11 18:10:44 +0200

    TrafficSignNet 0.0.8

--- TrafficSignNet/Loader.py
+++ TrafficSignNet/Loader.py
@@ -15,13 +15,14 @@ def load_split(basePath, csvPath):
     # it contains the CSV header), and shuffle the rows (otherwise
     # all examples of a particular class will be in sequential order)
     rows = open(csvPath).read().strip().split("\n")[1:]
+    #print(f"filenames: {rows[0]}")
     random.shuffle(rows)
 
     # loop over the rows of the CSV file
     for (i, row) in enumerate(rows):
         # check to see if we should show a status update
         if i > 0 and i % 1000 == 0:
-            print("[INFO] processed {} total images".format(i))
+            print("[INFO] processed {} total test images".format(i))
 
         # split the row into components and then grab the class ID
         # and image path
@@ -29,6 +30,67 @@ def load_split(basePath, csvPath):
 
         # derive the full path to the image file and load it
         imagePath = os.path.sep.join([basePath, imagePath])
+        image = io.imread(imagePath)
+
+        # resize the image to be 32x32 pixels, ignoring aspect ratio,
+        # and then perform Contrast Limited Adaptive Histogram
+        # Equalization (CLAHE)
+        image = transform.resize(image, (32, 32))
+        image = exposure.equalize_adapthist(image, clip_limit=0.1)
+
+        # update the list of data and labels, respectively
+        data.append(image)
+        labels.append(int(label))
+
+    # convert the data and labels to NumPy arrays
+    data = np.array(data)
+    labels = np.array(labels)
+
+    # return a tuple of the data and labels
+    return (data, labels)
+
+def load_dir(base):
+    # initialize the list of data and labels
+    rows = []
+    data = []
+    labels = []
+
+    # load the contents of the CSV file, remove the first line (since
+    # it contains the CSV header), and shuffle the rows (otherwise
+    # all examples of a particular class will be in sequential order)
+    #rows = open(csvPath).read().strip().split("\n")[1:]
+    #random.shuffle(rows)
+    path = os.path.sep.join([os.getcwd(), "dataset"])
+    path = os.path.sep.join([path, base])
+    dirpath, dirnames, filenames = next(os.walk(path), (None, [], []))  # [] if no file
+    #print(f"dirpath: {dirpath}")
+    #print(f"dirnames: {dirnames}")
+    #print(f"filenames: {filenames}")
+
+    for (d, dirname) in enumerate(dirnames):
+        dirPath = os.path.sep.join([path, dirname])
+        dirpath, dirnames, files = next(os.walk(dirPath), (None, [], []))  # [] if no file
+        #print(f"filenames: {len(files)}")
+        for (i, file) in enumerate(files):
+            # check to see if we should show a status update
+            filePath = os.path.sep.join([dirpath, file])
+            #print(filePath)
+            rows.append(dirname + ',' + filePath)
+
+    #print(f"filenames: {rows[0]}")
+    random.shuffle(rows)
+
+    # loop over the rows of the CSV file
+    for (i, row) in enumerate(rows):
+        # check to see if we should show a status update
+        if i > 0 and i % 1000 == 0:
+            print("[INFO] processed {} total train images".format(i))
+
+        # split the row into components and then grab the class ID
+        # and image path
+        (label, imagePath) = row.strip().split(",")[-2:]
+
+        # derive the full path to the image file and load it
         image = io.imread(imagePath)
 
         # resize the image to be 32x32 pixels, ignoring aspect ratio,
--- TrafficSignNet/myTrain.py
+++ TrafficSignNet/myTrain.py
@@ -1,10 +1,13 @@
 import argparse
+import matplotlib.pyplot as plt
 import os
 # to suppress warnings caused by cuda version
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow as tf
 print(tf.__version__)
 from Loader import *
+from pyimagesearch.trafficsignnet import TrafficSignNet
+from sklearn.metrics import classification_report
 
 #print("[INFO] constructing the argument parser and parse the arguments")
 ap = argparse.ArgumentParser()
@@ -18,7 +21,7 @@ args = vars(ap.parse_args())
 #print(f"args: {args}")
 
 #print("[INFO] initializing the number of epochs to train for, base learning rate, and batch size")
-NUM_EPOCHS = 30
+NUM_EPOCHS = 50#30
 INIT_LR = 1e-3
 BS = 64
 #print(f"NUM_EPOCHS: {NUM_EPOCHS}")
@@ -33,13 +36,15 @@ labelIds = [l.split(",")[0] for l in labels]
 #print(f"labelIds: {labelIds}")
 
 #print("[INFO] deriving the path to the training and testing CSV files")
-trainPath = os.path.sep.join([args["dataset"], "Train.csv"])
+#trainPath = os.path.sep.join([args["dataset"], "Train.csv"])
 testPath = os.path.sep.join([args["dataset"], "Test.csv"])
 #print(f"trainPath: {trainPath}")
 #print(f"testPath: {testPath}")
 
 print("[INFO] loading training and testing data...")
-(trainX, trainY) = load_split(args["dataset"], trainPath)
+(trainX, trainY) = load_dir('Train')
+#(testX, testY) = load_dir('Test')
+#(trainX, trainY) = load_split(args["dataset"], trainPath)
 (testX, testY) = load_split(args["dataset"], testPath)
 #print(f"trainX: {trainX}")
 #print(f"trainY: {trainY}")
@@ -60,15 +65,81 @@ testY = tf.keras.utils.to_categorical(testY, numLabels)
 #print(f"trainY: {trainY}")
 #print(f"testY: {testY}")
 
-print("[INFO] calculating the total number of images in each class")
+#print("[INFO] calculating the total number of images in each class")
 classTotals = trainY.sum(axis=0)
-print(f"classTotals: {classTotals}")
+#print(f"classTotals: {classTotals}")
 
 #print("[INFO] initializing a dictionary to store the class weights")
 classWeight = dict()
 #print(f"classWeight: {classWeight}")
 
-print("[INFO] looping over all classes and calculating the class weight")
+#print("[INFO] looping over all classes and calculating the class weight")
 for i in range(0, len(classTotals)):
     classWeight[i] = classTotals.max() / classTotals[i]
-print(f"classWeight: {classWeight}")
+#print(f"classWeight: {classWeight}")
+
+#print("[INFO] constructing the image generator for data augmentation")
+aug = tf.keras.preprocessing.image.ImageDataGenerator(
+    rotation_range=10,
+    zoom_range=0.15,
+    width_shift_range=0.1,
+    height_shift_range=0.1,
+    shear_range=0.15,
+    horizontal_flip=False,
+    vertical_flip=False,
+    fill_mode="nearest")
+#print(f"aug: {aug}")
+
+# initialize the optimizer and compile the model
+print("[INFO] compiling model...")
+opt = tf.keras.optimizers.Adam(
+    learning_rate=INIT_LR,
+    decay=INIT_LR / (NUM_EPOCHS * 0.5))
+#print(f"opt: {opt}")
+model = TrafficSignNet.build(
+    width=32,
+    height=32,
+    depth=3,
+    classes=numLabels)
+#print(f"model: {model}")
+model.compile(
+    loss="categorical_crossentropy",
+    optimizer=opt,
+    metrics=["accuracy"])
+#print(f"model: {model}")
+
+# train the network
+print("[INFO] training network...")
+H = model.fit(
+    aug.flow(trainX, trainY, batch_size=BS),
+    validation_data=(testX, testY),
+    steps_per_epoch=trainX.shape[0] // BS,
+    epochs=NUM_EPOCHS,
+    class_weight=classWeight,
+    verbose=1)
+
+# evaluate the network
+print("[INFO] evaluating network...")
+predictions = model.predict(testX, batch_size=BS)
+print(classification_report(
+    testY.argmax(axis=1),
+    predictions.argmax(axis=1),
+    target_names=labelNames))
+
+# save the network to disk
+print("[INFO] serializing network to '{}'...".format(args["model"]))
+model.save(args["model"])
+
+# plot the training loss and accuracy
+N = np.arange(0, NUM_EPOCHS)
+plt.style.use("ggplot")
+plt.figure()
+plt.plot(N, H.history["loss"], label="train_loss")
+plt.plot(N, H.history["val_loss"], label="val_loss")
+plt.plot(N, H.history["accuracy"], label="train_acc")
+plt.plot(N, H.history["val_accuracy"], label="val_acc")
+plt.title("Training Loss and Accuracy on Dataset")
+plt.xlabel("Epoch #")
+plt.ylabel("Loss/Accuracy")
+plt.legend(loc="lower left")
+plt.savefig(args["plot"])
