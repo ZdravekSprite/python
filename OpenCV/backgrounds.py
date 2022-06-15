6794bea889b1fd9756e0c22db05e7853c714d0b9
Zdravko Šplajt <zdravek.sprite@gmail.com>
2022-06-15 21:33:34 +0200

    OpenCV 0.0.19

--- OpenCV/backgrounds.py
+++ OpenCV/backgrounds.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import numpy as np
+from scipy import ndimage
+import random
 
 
 def get_random_crop(image, crop_height, crop_width, padding):
@@ -19,24 +21,24 @@ def combine_images(overlay, background):
     bpy = int((background_height - overlay_height)/2)
     # background_padding_x
     bpx = int((background_width - overlay_width)/2)
-
     for y in range(overlay_height):
         for x in range(overlay_width):
             overlay_color = overlay[y, x, :3]
             # 4th element is the alpha channel, convert from 0-255 to 0.0-1.0
             overlay_alpha = overlay[y, x, 3] / 255
-
-            # get the color from the background image
             background_color = random_crop[bpy+y, bpx+x]
-
-            # combine the background color and the overlay color weighted by alpha
-            composite_color = background_color * \
+            combine_color = background_color * \
                 (1 - overlay_alpha)+overlay_color*overlay_alpha
-
             # update the background image in place
-            background[bpy+y, bpx+x] = composite_color
-
+            background[bpy+y, bpx+x] = combine_color
     return background
+
+
+def rotate_image(image, angle):
+    deg = random.random()*angle
+    pos = random.random()-0.5
+    rotated = ndimage.rotate(image, deg*pos)
+    return rotated
 
 
 rootPath = "../datasets/"
@@ -62,12 +64,16 @@ for (o, overlay_file) in enumerate(overlay_files):
         background_name, _ = background_file.split(".")[-2:]
         #print("background file: " + background_name)
 
-        height, width = overlay.shape[:2]
-        random_crop = get_random_crop(background, height, width, 5)
-
         targetPath = rootPath+"test/combined/"
         targetName = overlay_name+"_"+background_name
 
-        v01 = combine_images(overlay, random_crop)
+        height, width = overlay.shape[:2]
+        random_crop = get_random_crop(background, height, width, 5)
+        version = combine_images(overlay, random_crop)
+        cv2.imwrite(targetPath+targetName+"_v01.png", version)
 
-        cv2.imwrite(targetPath+targetName+"_v01.png", v01)
+        rotate = rotate_image(overlay, 10)
+        height, width = rotate.shape[:2]
+        random_crop = get_random_crop(background, height, width, 5)
+        version = combine_images(rotate, random_crop)
+        cv2.imwrite(targetPath+targetName+"_v02.png", version)
--- README.md
+++ README.md
@@ -45,7 +45,7 @@ pip install --upgrade opencv-python
 pip install --upgrade opencv-contrib-python
 
 git add .
-git commit -am "OpenCV 0.0.18"
+git commit -am "OpenCV 0.0.19"
 git push
 ```
 
