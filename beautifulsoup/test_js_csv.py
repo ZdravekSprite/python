862c823887cdc08a2cc7402fd653418b143d9a3b
Zdravko Šplajt <zdravek.sprite@gmail.com>
2025-02-01 15:02:46 +0100

    bs 2

--- beautifulsoup/test_js_csv.py
+++ beautifulsoup/test_js_csv.py
@@ -1,21 +1,41 @@
+import os
 from selenium import webdriver 
 from bs4 import BeautifulSoup 
 import time
 from selenium.webdriver.common.by import By
+# importing csv module
+import csv
+
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+CSV_FILE = os.path.sep.join([SCRIPT_DIR, 'EUROJACKPOT.csv'])
 
 url = "https://www.lutrija.hr/hl/rezultati/eurojackpot"
+kola = []
+
+# csv file name
+filename = CSV_FILE
+
+# reading csv file
+with open(filename, 'r', encoding="windows 1252") as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile, delimiter=";")
+
+    # extracting each data row one by one
+    for row in csvreader:
+        kola.append(row)
 
 # Set up the browser 
 options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
-#driver = webdriver.Chrome(options=options)
-driver = webdriver.Chrome()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+#driver = webdriver.Chrome()
 #driver = webdriver.Firefox()
 
 # Navigate to URL
 driver.get(url)
 time.sleep(5)
 
+"""
 while True:
     # Click
     btn_elements = driver.find_elements(By.CLASS_NAME, "btn-default")
@@ -25,14 +45,16 @@ while True:
         time.sleep(5)
     else:
         break
+"""
 
 # Get the page source after the JavaScript has executed 
 page_source = driver.page_source 
  
 # Use BeautifulSoup to parse the HTML 
 soup = BeautifulSoup(page_source, 'html.parser') 
- 
-kola = []
+
+nova_kola = []
+
 for div in soup.find_all('div', class_='result-numbers'):
     kolo = []
     drawDateTime_div = div.find('div', class_='drawDateTime')
@@ -50,10 +72,24 @@ for div in soup.find_all('div', class_='result-numbers'):
         #print(li.text)
         kolo.append(li.text)
     #print(div.text)
-    kola.append(kolo)
+    if kolo[1] != kola[0][1]:
+        nova_kola.append(kolo)
+    else:
+        break
 
 # Close the browser 
 driver.quit()
 
-for k in kola:
+for k in nova_kola:
     print('kolo',k)
+
+nova_kola = nova_kola+kola
+
+# writing to csv file
+with open(filename, 'w', encoding="windows 1252") as csvfile:
+    # creating a csv writer object
+    csvwriter = csv.writer(csvfile, delimiter=";",lineterminator='\n')
+    # writing the fields
+    #csvwriter.writerow(fields)
+    # writing the data rows
+    csvwriter.writerows(nova_kola)
