feff9fcf6ad515437b8ebca5af43e4177dcd5122
Zdravko Šplajt <zdravek.sprite@gmail.com>
2025-06-05 06:16:52 +0200

    monero 125

--- monero/not_zero.py
+++ monero/not_zero.py
@@ -7,6 +7,137 @@ from helper_file import *
 from monero import base58
 from config import *
 
+af_fieldnames = ['address','hex','block','outputs']
+
+def paths_to(to_path,file):
+    path_to_dict = os.path.sep.join([to_path,'address_csv',file])
+    if not os.path.isfile(path_to_dict):
+        with open(path_to_dict.lower(), 'w', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=af_fieldnames)
+            writer.writeheader()
+
+    path_to_zero = os.path.sep.join([to_path,'not_zero',file])
+    if not os.path.isfile(path_to_zero):
+        with open(path_to_zero.lower(), 'w') as fp:
+            pass
+    return (path_to_dict,path_to_zero)
+
+def zero_append(zero_path,hexs):
+    with open(zero_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(hexs)
+
+def create_zero_list(zero_path):
+    csv_list = []
+    with open(zero_path, newline='') as csvfile:
+        reader = csv.reader(csvfile)
+        for row in reader:
+            csv_list.append(row[0])
+    return csv_list
+
+def create_addr_dict(dict_path):
+    csv_dict = {}
+    with open(dict_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile, fieldnames=af_fieldnames)
+        for row in reader:
+            if row['address'] != 'address': csv_dict = add_row_to_dict(csv_dict, row)
+    return csv_dict
+
+def add_row_to_dict(dict_,row_):
+    if row_['address'] in dict_.keys():
+        if dict_[row_['address']] != row_:
+            '''
+            print('DIFFER:',dict_[row_['address']]['hex'],dict_[row_['address']]['address'],
+                  'old:',dict_[row_['address']]['block'],dict_[row_['address']]['outputs'],
+                  'new:',row_['block'],row_['outputs'],
+                  " "*10, end='\r')
+            '''
+            if dict_[row_['address']]['hex'] != row_['hex']:
+                print('DIFFER:',dict_[row_['address']]['address'],
+                  'old:',dict_[row_['address']]['hex'],
+                  'new:',row_['hex'],
+                  " "*10)
+            if dict_[row_['address']]['address'] != row_['address']:
+                print('DIFFER:',dict_[row_['address']]['hex'],
+                  'old:',dict_[row_['address']]['address'],
+                  'new:',row_['address'],
+                  " "*10)
+            if int(dict_[row_['address']]['block']) < int(row_['block']):
+                dict_[row_['address']] = row_
+                print('DIFFER:',dict_[row_['address']]['hex'],
+                  'old:',dict_[row_['address']]['block'],row_['outputs'],
+                  'new:',row_['block'],row_['outputs'],
+                  " "*10)
+    else:
+        dict_[row_['address']] = row_
+    return dict_
+
+def dict_write(dict_path,dict_):
+    with open(dict_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=af_fieldnames)
+        writer.writeheader()
+        writer.writerows(dict_.values())
+
+def merge_dict(from_path,to_path):
+    from_files = next(os.walk(from_path), (None, None, []))[2]
+    count=0
+    for file in from_files:
+
+        path_to_dict, path_to_zero = paths_to(to_path,file)
+        zero_list = create_zero_list(path_to_zero)
+        csv_dict = create_addr_dict(path_to_dict)
+        if len(zero_list) == len(csv_dict):
+            count+=len(zero_list)
+        else:
+            print(len(zero_list),len(csv_dict),file)
+        zero_append_list = []
+
+        path_from_dict = os.path.sep.join([from_path,file])
+        with open(path_from_dict, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                csv_dict = add_row_to_dict(csv_dict, row)
+                if row['hex'] not in zero_list:
+                    count+=1
+                    zero_append_list.append([row['hex']])
+        zero_append(path_to_zero,zero_append_list)
+        dict_write(path_to_dict,csv_dict)
+        print("now: ",dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file, count, " "*10, end='\r')
+    print()
+
+def merge_zero(from_path,to_path):
+    from_files = next(os.walk(from_path), (None, None, []))[2]
+    count=0
+    for file in from_files:
+
+        path_to_dict, path_to_zero = paths_to(to_path,file)
+        zero_list = create_zero_list(path_to_zero)
+        csv_dict = create_addr_dict(path_to_dict)
+        if len(zero_list) == len(csv_dict):
+            count+=len(zero_list)
+        else:
+            print(len(zero_list),len(csv_dict),file)
+        zero_append_list = []
+
+        path_from_zero = os.path.sep.join([from_path,file])
+        with open(path_from_zero, newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            for row in reader:
+                dict_row = {
+                    'address':str(public_address(row[0])),
+                    'hex':row[0],
+                    'block':'0',
+                    'outputs':'0'
+                }
+                csv_dict = add_row_to_dict(csv_dict, dict_row)
+                if row[0] not in zero_list:
+                    count+=1
+                    zero_append_list.append(row[0])
+        zero_append(path_to_zero,zero_append_list)
+        dict_write(path_to_dict,csv_dict)
+        print("now: ",dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), file, count, " "*10, end='\r')
+    print()
+
 def format(from_path,to_path):
     count=0
     from_ = next(os.walk(from_path), (None, None, []))[2]
@@ -90,9 +221,13 @@ def rnd_check(to_path, debug = False):
 
 if __name__ == '__main__':
     print(__file__)
-    to_path = "c:\\monero\\not_zero"
+    from_dict_path = "c:\\monero\\address_csv_1" #"c:\\monero\\address_csv"
+    from_zero_path = "c:\\monero\\not_zero_1" #"c:\\monero\\address_csv"
+    to_path = "c:\\monero"
+    count=0
     print('start:',dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
-    #from_path = "c:\\monero\\address_csv"
+    #merge_dict(from_dict_path,to_path)
+    #merge_zero(from_zero_path,to_path)
     #format(from_path,to_path)
     '''
     test_addr = AddressSlow(test_address_row['hex'])
@@ -107,6 +242,7 @@ if __name__ == '__main__':
         test_addr.address_file()
         )
     '''
+    '''
     count = 0
     temp_count = 0
     start_time = dt.datetime.now()
@@ -120,4 +256,5 @@ if __name__ == '__main__':
             temp_count = 0
             start_time = dt.datetime.now()
     print()
+    '''
     print('end:  ',dt.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),' '*10)
